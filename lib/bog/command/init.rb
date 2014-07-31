6aebde0cb4681de3a035028eb0d4a89f9c6773ff
Sam Pointer <sam.pointer@opsunit.com>
2014-07-31 14:09:26 +0100

    grab arguments before slop parses them

--- lib/bog/command/init.rb
+++ lib/bog/command/init.rb
@@ -5,7 +5,7 @@ module Bog
     class Init
       def self.execute(profile, options)
         create_skel
-        # FIXME: make use of profile.path instead; get passwd a Profile object
+        # FIXME: make use of profile.root instead; get passed a Profile object
         profile_path = File.join(File.expand_path('~/.bog'), 'profiles', profile.to_s)
         Dir.mkdir(profile_path) unless File.directory?(profile_path)
 
